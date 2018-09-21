b8c3dd4aec5b4e8d12e39502bdf7222abb0205f0
Ani Gomez <anig1496@gmail.com>
2018-09-21 12:42:33 -0400

    menu functiality working, missing navigation

---
+++ FKADapp/src/components/common/Menu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, Modal, Image, TouchableOpacity } from 'react-native';
+
+const Menu = ({ visible, onPress }) => {
+	return (
+		<Modal
+			visible={visible}
+			transparent
+			animationType="fade"
+			onRequestClose={() => {}}
+		>
+			<TouchableOpacity onPress={onPress}>
+				<View style={styles.containerStyle}>
+					<TouchableOpacity onPress={onPress}>
+						<Image
+							style={{ height: 55 }}
+							source={require('../../images/menuIcon.png')}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Text style={styles.textStyle}>Dashboard</Text>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Text style={styles.textStyle}>New Order</Text>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Text style={styles.textStyle}>My Orders</Text>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Text style={styles.textStyle}>Settings</Text>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Text style={styles.textStyle}>Log Out</Text>
+					</TouchableOpacity>
+				</View>
+			</TouchableOpacity>
+		</Modal>
+	);
+};
+
+const styles = {
+	textStyle: {
+		fontSize: 20,
+		fontFamily: 'AppleGothic',
+		color: 'white',
+		padding: 5,
+		marginLeft: 5
+	},
+	containerStyle: {
+		width: '60%',
+		height: '100%',
+		backgroundColor: '#3982B6'
+	}
+};
+
+export default Menu;
