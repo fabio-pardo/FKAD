1ac7fce15fe777878600c9eb730d8311d366ea1d
Ani Gomez <anig1496@gmail.com>
2018-09-21 10:02:56 -0400

    save changes with react-native-route-flex

--- FKADapp/src/components/common/Header.js
+++ FKADapp/src/components/common/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Drawer from 'react-native-drawer';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 const Header = ({ headerTitle }) => {
