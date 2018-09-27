fce9428bdb5fe3b4a784335f440f56cae2daf614
Ani Gomez <anig1496@gmail.com>
2018-09-27 10:00:11 -0400

    New Order pick up from complete

--- FKADapp/src/components/newOrder/CreateNewOrder.js
+++ FKADapp/src/components/newOrder/CreateNewOrder.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-import { Header } from '../common';
+import { Header, Button, Input, InputThree } from '../common';
 
 class CreateNewOrder extends Component {
 	render() {
 		return (
-			<View>
+			<View style={{ backgroundColor: 'white' }}>
 				<Header headerTitle="New Order" />
+				<View style={styles.containerStyle}>
+					<Text style={styles.textStyle}>Pick Up From:</Text>
+					<View style={{ marginTop: 15 }}>
+						<Input label="Grocery Place Name" />
+						<Input label="Street Address" />
+						<View
+							style={{
+								flexDirection: 'row',
+								justifyContent: 'space-between'
+							}}
+						>
+							<InputThree label="City" />
+							<InputThree label="State" />
+							<InputThree label="Zipcode" />
+						</View>
+						<Input label="Order Number" />
+					</View>
+					<View style={styles.buttonStyle}>
+						<Button>Next  &#8827;&#8827;</Button>
+					</View>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = {
+	containerStyle: {
+		marginTop: 20,
+		margin: 10,
+		height: '100%'
+	},
+	textStyle: {
+		fontSize: 20,
+		fontFamily: 'AppleGothic',
+		color: '#3982B6'
+	},
+	buttonStyle: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginTop: 30
+	}
+};
 
 export default CreateNewOrder;
