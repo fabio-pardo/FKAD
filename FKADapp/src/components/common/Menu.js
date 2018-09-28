4fd9143e73a8a315722c8c7e2b32dc6a64f7de55
Fabio Pardo <fabs@Fabios-MacBook-Pro.local>
2018-09-28 16:07:49 -0400

    Logout button almost functional. Menu needs to be hidden.

--- FKADapp/src/components/common/Menu.js
+++ FKADapp/src/components/common/Menu.js
@@ -1,50 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Modal, Image, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Confirm } from './Confirm';
 
-const Menu = ({ visible, onPress }) => {
-	return (
-		<Modal
-			visible={visible}
-			transparent
-			animationType="fade"
-			onRequestClose={() => {}}
-		>
-			<TouchableOpacity onPress={onPress}>
-				<View style={styles.containerStyle}>
-					<TouchableOpacity onPress={onPress}>
-						<Image
-							style={{ height: 55 }}
-							source={require('../../images/menuIcon.png')}
-						/>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Actions.dashboard();
-						}}
-					>
-						<Text style={styles.textStyle}>Dashboard</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Actions.createNewOrder();
-						}}
-					>
-						<Text style={styles.textStyle}>New Order</Text>
-					</TouchableOpacity>
-					<TouchableOpacity>
-						<Text style={styles.textStyle}>My Orders</Text>
-					</TouchableOpacity>
-					<TouchableOpacity>
-						<Text style={styles.textStyle}>Settings</Text>
-					</TouchableOpacity>
-					<TouchableOpacity>
-						<Text style={styles.textStyle}>Log Out</Text>
-					</TouchableOpacity>
-				</View>
-			</TouchableOpacity>
-		</Modal>
-	);
+class Menu extends Component {
+	state = { showModal: false }
+
+
+	onLogoutAccept() {
+		//Should sign out UID from AWS
+		//Should return to main menu
+		Actions.login();
+		//BUG: Menu Doesn't Close On LogOutAccept
+	}
+
+	onLogoutDecline() {
+		//BUG: MENU Doesn't Close on LogOutDeny (It Should)
+		this.setState({ showModal: false });
+	}
+
+	hideNavBar() {
+		this.props.onPress();
+	}
+
+	render() {
+		const { onPress, visible } = this.props;
+
+		return (
+			<Modal
+				visible={visible}
+				transparent
+				animationType="fade"
+				onRequestClose={() => {}}
+			>
+				<TouchableOpacity onPress={onPress}>
+					<View style={styles.containerStyle}>
+						<TouchableOpacity onPress={onPress}>
+							<Image
+								style={{ height: 55 }}
+								source={require('../../images/menuIcon.png')}
+							/>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => {
+								this.hideNavBar();
+								Actions.dashboard();
+							}}
+						>
+							<Text style={styles.textStyle}>Dashboard</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => {
+								this.hideNavBar();
+								Actions.createNewOrder();
+							}}
+						>
+							<Text style={styles.textStyle}>New Order</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => {
+								this.hideNavBar();
+							}}
+						>
+							<Text style={styles.textStyle}>My Orders</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => {
+								this.hideNavBar();
+							}}
+						>
+							<Text style={styles.textStyle}>Settings</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => {
+							this.setState({ showModal: !this.state.showModal })
+						}}>
+							<Text style={styles.textStyle}>Log Out</Text>
+						</TouchableOpacity>
+
+						<Confirm
+							visible={this.state.showModal}
+							onAccept={this.onLogoutAccept.bind(this)}
+							onDecline={this.onLogoutDecline.bind(this)}
+						>
+							Are You Sure You Want To Log Out?
+						</Confirm>
+					</View>
+				</TouchableOpacity>
+			</Modal>
+		);
+	}
 };
 
 const styles = {
