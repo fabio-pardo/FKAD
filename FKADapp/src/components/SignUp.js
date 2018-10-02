47a9cdafd759fc4e10b69e8ce59fa44c6dd7398c
Ani Gomez <anig1496@gmail.com>
2018-10-02 16:01:40 -0400

    Fix merge

--- FKADapp/src/components/SignUp.js
+++ FKADapp/src/components/SignUp.js
@@ -1,129 +1,98 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Content, Container } from  'native-base'
+import { Content, Container } from 'native-base';
 import {
   InputLogIn,
   Input,
   InputTwo,
   InputThree,
   HeaderWithoutMenu,
-<<<<<<< HEAD
-<<<<<<< HEAD
   InputV2,
   Button
-=======
-  InputV2
->>>>>>> master
-=======
-  InputV2,
-  Button
->>>>>>> 75d0cace660192265e6432085a8ae5377087fb12
 } from './common';
 
 class SignUp extends Component {
-
   render() {
     const {
       singleInputStyle,
       twoInputStyle,
       threeInputStyle,
-      rowInputViewStyle,
+      rowInputViewStyle
     } = styles;
 
-    return(
+    return (
       <Container>
-        <View style={{backgroundColor: 'white', height: '100%'}}>
+        <View style={{ backgroundColor: 'white', height: '100%' }}>
           <HeaderWithoutMenu headerTitle="SignUp" />
-<<<<<<< HEAD
-<<<<<<< HEAD
-          <Content style={{margin: 20, marginTop: 0}}>
-=======
-          <Content style={{margin: 20}}>
->>>>>>> master
-=======
-          <Content style={{margin: 20, marginTop: 0}}>
-
->>>>>>> 75d0cace660192265e6432085a8ae5377087fb12
+          <Content style={{ margin: 20, marginTop: 0 }}>
             <View style={rowInputViewStyle}>
-              <InputV2
-                label='First Name' style={twoInputStyle}
-              />
-              <InputV2
-                label="Last Name" style={twoInputStyle}
-              />
+              <InputV2 label="First Name" style={twoInputStyle} />
+              <InputV2 label="Last Name" style={twoInputStyle} />
             </View>
             <View>
-              <InputV2 label='G-Mail' style={singleInputStyle} />
-              <InputV2 label='Phone Number' style={singleInputStyle} />
-              <InputV2 label='Street Address' style={singleInputStyle} />
+              <InputV2 label="G-Mail" style={singleInputStyle} />
+              <InputV2 label="Phone Number" style={singleInputStyle} />
+              <InputV2 label="Street Address" style={singleInputStyle} />
             </View>
             <View style={rowInputViewStyle}>
-              <InputV2 label='City' style={threeInputStyle} />
-              <InputV2 label='State' style={threeInputStyle} />
-              <InputV2 label='Zip' style={threeInputStyle} />
+              <InputV2 label="City" style={threeInputStyle} />
+              <InputV2 label="State" style={threeInputStyle} />
+              <InputV2 label="Zip" style={threeInputStyle} />
             </View>
             <View>
-              <InputV2 label='Enter Password' style={singleInputStyle} />
-              <InputV2 label='Confirm Password' style={singleInputStyle} />
+              <InputV2 label="Enter Password" style={singleInputStyle} />
+              <InputV2 label="Confirm Password" style={singleInputStyle} />
             </View>
-<<<<<<< HEAD
-<<<<<<< HEAD
             <Button>Confirm</Button>
-=======
->>>>>>> master
-=======
-            <Button>Confirm</Button>
->>>>>>> 75d0cace660192265e6432085a8ae5377087fb12
           </Content>
         </View>
       </Container>
-
-    )
+    );
   }
 }
 
 const styles = {
-  rowInputViewStyle:{
+  rowInputViewStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'flex-start',
+    alignItems: 'flex-start'
   },
   singleInputStyle: {
     color: 'white',
-		backgroundColor: '#ADCBE0',
-		fontFamily: 'AppleGothic',
-		paddingRight: 5,
-		paddingLeft: 5,
-		fontSize: 17,
-		lineHeight: 23,
-		flex: 1,
-		width: '100%',
-		alignSelf: 'stretch'
+    backgroundColor: '#ADCBE0',
+    fontFamily: 'AppleGothic',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 17,
+    lineHeight: 23,
+    flex: 1,
+    width: '100%',
+    alignSelf: 'stretch'
   },
   twoInputStyle: {
-		color: 'white',
-		backgroundColor: '#ADCBE0',
-		fontFamily: 'AppleGothic',
-		paddingRight: 5,
-		paddingLeft: 5,
-		fontSize: 17,
-		lineHeight: 23,
-		flex: 1,
-		width: 160,
-		alignSelf: 'stretch'
-	},
+    color: 'white',
+    backgroundColor: '#ADCBE0',
+    fontFamily: 'AppleGothic',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 17,
+    lineHeight: 23,
+    flex: 1,
+    width: 160,
+    alignSelf: 'stretch'
+  },
   threeInputStyle: {
     color: 'white',
-		backgroundColor: '#ADCBE0',
-		fontFamily: 'AppleGothic',
-		paddingRight: 5,
-		paddingLeft: 5,
-		fontSize: 17,
-		lineHeight: 23,
-		flex: 1,
-		width: 100,
-		alignSelf: 'stretch'
+    backgroundColor: '#ADCBE0',
+    fontFamily: 'AppleGothic',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 17,
+    lineHeight: 23,
+    flex: 1,
+    width: 100,
+    alignSelf: 'stretch'
   }
-}
+};
 
 export default SignUp;
