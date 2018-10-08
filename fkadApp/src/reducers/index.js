f5fa1def5acc81803c20de7182ecf8d6809097d5
Fabio Pardo <fpardo1023@gmail.com>
2018-10-08 18:31:46 -0400

    state in object street and name are clearing

--- fkadApp/src/reducers/index.js
+++ fkadApp/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
 import AuthReducer from './AuthReducer';
 import NewOrderReducer from './NewOrderReducer';
+import NewSignUpReducer from './NewSignUpReducer';
 
 export default combineReducers({
 	auth: AuthReducer,
-	newOrder: NewOrderReducer
+	newOrder: NewOrderReducer,
+	signUp: NewSignUpReducer
 });
